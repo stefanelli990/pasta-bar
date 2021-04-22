d60a6e7988eedbad825de6ceb196f60a4ee74a58
stefanelli990 <stefan.mohenski@gmail.com>
2021-04-22 14:09:18 +0200

    last change

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ const navLinks = document.querySelectorAll('.navbar__nav-link');
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 500) {
         navbar.classList.add('active');
-        navLinks.forEach(navLink => {
-            navLink.classList.add('active');
-        });
     } else {
         navbar.classList.remove('active');
-        navLinks.forEach(navLink => {
-            navLink.classList.remove('active');
-        });
     }
 });
