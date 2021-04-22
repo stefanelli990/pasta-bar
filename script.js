7287f5f7cf9af2a39dbd70621319d128f17101b0
stefanelli990 <stefan.mohenski@gmail.com>
2021-04-22 20:33:24 +0200

    last change

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const navbar = document.querySelector('.navbar');
 const navLinks = document.querySelectorAll('.navbar__nav-link');
+const toggle = document.querySelector('.navbar__toggle');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
 
+
+//scroll navbar animation
 window.addEventListener('scroll', () => {
     if(window.pageYOffset > 500) {
         navbar.classList.add('active');
@@ -8,3 +12,13 @@ window.addEventListener('scroll', () => {
         navbar.classList.remove('active');
     }
 });
+
+//hamburger menu toggle
+toggle.addEventListener('click', () => {
+  
+    if (hamburgerMenu.style.maxHeight) {
+      hamburgerMenu.style.maxHeight = null;
+    } else {
+      hamburgerMenu.style.maxHeight = hamburgerMenu.scrollHeight + "px";
+    } 
+  });
